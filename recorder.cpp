ff14fbf2a970ab04c4a4b4957446ea5f6da31c95
Alex Goodman <alexthegoodman@gmail.com>
2023-07-23 22:41:32 -0400

    successful window recording

--- recorder.cpp
+++ recorder.cpp
@@ -106,6 +106,9 @@ void SetupDesktopDuplication(HWND sharedHwnd) {
     int windowWidth = windowRect.right - windowRect.left;
     int windowHeight = windowRect.bottom - windowRect.top;
 
+    windowWidth = (windowWidth % 2 == 0) ? windowWidth : windowWidth - 1;
+    windowHeight = (windowHeight % 2 == 0) ? windowHeight : windowHeight - 1;
+
     // *** Setup FFmpeg *** //
     AVFormatContext *encoderFormatCtx = NULL;
     avformat_alloc_output_context2(&encoderFormatCtx, NULL, NULL, outputFilename);
@@ -113,6 +116,8 @@ void SetupDesktopDuplication(HWND sharedHwnd) {
         g_print("Could not create output context\n");
         return;
     }
+
+    encoderFormatCtx->oformat = av_guess_format("mp4", NULL, NULL);
 
     // Setup the codec
     AVStream *encoderStream = avformat_new_stream(encoderFormatCtx, NULL);
@@ -136,7 +141,7 @@ void SetupDesktopDuplication(HWND sharedHwnd) {
     g_print("Setting up codec context...\n");
     g_print("Width: %d Height: %d\n", windowWidth, windowHeight);
 
-    encoderCodecCtx->bit_rate = 10000; // 10Mbps / 10,000Kbps
+    encoderCodecCtx->bit_rate = 5000 * 1000; // 5,000 Kbps = 5,000,000 bps
     encoderCodecCtx->width = windowWidth;
     encoderCodecCtx->height = windowHeight;
     encoderCodecCtx->time_base = (AVRational){1, 30}; // 30FPS
@@ -146,6 +151,8 @@ void SetupDesktopDuplication(HWND sharedHwnd) {
     encoderCodecCtx->pix_fmt = AV_PIX_FMT_YUV420P;
 
     encoderStream->time_base = encoderCodecCtx->time_base;
+    encoderStream->r_frame_rate = encoderCodecCtx->framerate;
+    encoderStream->avg_frame_rate = encoderCodecCtx->framerate;
 
     // Step 3: Open the codec and add the stream info to the format context
     if (avcodec_open2(encoderCodecCtx, NULL, NULL) < 0) {
@@ -177,7 +184,8 @@ void SetupDesktopDuplication(HWND sharedHwnd) {
     // Determine the duration of each frame at 30 FPS.
     auto frameDuration = std::chrono::duration_cast<std::chrono::milliseconds>(
         std::chrono::duration<double>(1.0 / 30.0));
-
+    int frameIndex = 0;
+    
     while (continueCapturing) {
         // Time point when we start capturing this frame.
         auto frameStart = std::chrono::high_resolution_clock::now();
@@ -194,7 +202,6 @@ void SetupDesktopDuplication(HWND sharedHwnd) {
             continue;
         }
         else if (FAILED(hr)) {
-            // An error occurred, handle it here
             g_print("An error occurred while capturing\n");
             break;
         }
@@ -203,8 +210,42 @@ void SetupDesktopDuplication(HWND sharedHwnd) {
         IDXGISurface1* desktopSurface = nullptr;
         hr = desktopResource->QueryInterface(__uuidof(IDXGISurface1), (void**)&desktopSurface);
 
+        if (FAILED(hr)) {
+            g_print("An error occurred while querying the IDXGISurface1 interface\n");
+        }
+        
+        ID3D11Texture2D* pTexture = nullptr;
+        // Convert the IDXGISurface1 to ID3D11Texture2D
+        hr = desktopSurface->QueryInterface(__uuidof(ID3D11Texture2D), reinterpret_cast<void **>(&pTexture));
+
+        if (FAILED(hr)) {
+            g_print("An error occurred while querying the ID3D11Texture2D interface\n");
+        }
+
+        // copy the texture to read-allowed space
+        D3D11_TEXTURE2D_DESC desc;
+        pTexture->GetDesc(&desc);
+
+        desc.CPUAccessFlags = D3D11_CPU_ACCESS_READ;
+        desc.Usage = D3D11_USAGE_STAGING;
+        desc.BindFlags = 0;  // We don't need to bind this texture to the pipeline.
+        desc.MiscFlags = 0;
+        desc.MipLevels = 1;
+        desc.ArraySize = 1;
+        desc.SampleDesc.Count = 1;
+        desc.SampleDesc.Quality = 0;
+
+        ID3D11Texture2D* cpuTexture = nullptr;
+        hr = d3dDevice->CreateTexture2D(&desc, nullptr, &cpuTexture);
+        if (FAILED(hr)) {
+            g_print("An error occurred while creating texture\n");
+        }
+
+        // Now we have a texture that we can map. Copy the data from the original texture.
+        d3dDeviceContext->CopyResource(cpuTexture, pTexture);
+
         D3D11_MAPPED_SUBRESOURCE mappedRect; // mapped rect
-        hr = d3dDeviceContext->Map(desktopSurface, 0, D3D11_MAP_READ, 0, &mappedRect);
+        hr = d3dDeviceContext->Map(cpuTexture, 0, D3D11_MAP_READ, 0, &mappedRect);
 
         if (FAILED(hr)) {
             g_print("Failed to map desktop surface image\n");
@@ -228,11 +269,17 @@ void SetupDesktopDuplication(HWND sharedHwnd) {
                     memcpy(destRow, srcRow + windowRect.left * 4, windowWidth * 4);
                 }
 
-                dxgiSurface->Unmap();
+                desktopSurface->Unmap();
+
+                // check that windowBuffer has valid data
+                if (windowBuffer == nullptr) {
+                    g_print("windowBuffer is null\n");
+                    return;
+                }
 
                 // Now you can use windowBuffer for encoding with FFmpeg...
-                AVPixelFormat* captured_pix_fmt = AV_PIX_FMT_BGRA;
-                AVPixelFormat* encoder_pix_fmt = AV_PIX_FMT_YUV420P;
+                AVPixelFormat captured_pix_fmt = AV_PIX_FMT_BGRA;
+                AVPixelFormat encoder_pix_fmt = AV_PIX_FMT_YUV420P;
                 int bytes_per_pixel = 4;
 
                 // Convert the captured window buffer to the proper pixel format expected by the encoder
@@ -240,24 +287,68 @@ void SetupDesktopDuplication(HWND sharedHwnd) {
                     windowWidth, windowHeight, encoder_pix_fmt, SWS_BICUBIC, NULL, NULL, NULL);
                 uint8_t *src_data[4], *dst_data[4];
                 int src_linesize[4], dst_linesize[4];
-                av_image_alloc(src_data, src_linesize, windowWidth, windowHeight, captured_pix_fmt, 1);
-                av_image_alloc(dst_data, dst_linesize, windowWidth, windowHeight, encoder_pix_fmt, 1);
+                int ret = av_image_alloc(src_data, src_linesize, windowWidth, windowHeight, captured_pix_fmt, 1);
+
+                if (ret < 0) {
+                    g_print("Error allocating image 1\n");
+                }
+
+                ret = av_image_alloc(dst_data, dst_linesize, windowWidth, windowHeight, encoder_pix_fmt, 1);
+
+                if (ret < 0) {
+                    g_print("Error allocating image 2\n");
+                }
+
                 memcpy(src_data[0], windowBuffer, windowWidth * windowHeight * bytes_per_pixel);
                 sws_scale(swsCtx, src_data, src_linesize, 0, windowHeight, dst_data, dst_linesize);
+
+                // check dst_data
+                for (int i = 0; i < 4; i++) {
+                    if (dst_data[i] == nullptr) {
+                        // [3] should by null for YUV
+                        g_print("dst_data[%d] is null\n", i);
+                    }
+                }
+
+                // check dst_linesize
+                for (int i = 0; i < 4; i++) {
+                    if (dst_linesize[i] == 0) {
+                        // [3] should by null for YUV here also
+                        g_print("dst_linesize[%d] is 0\n", i);
+                    }
+                    else {
+                        g_print("dst_linesize[%d] = %d\n", i, dst_linesize[i]);
+                    }
+                }
+
+                g_print("Allocating frame...\n");
 
                 // Create AVFrame and set its data and linesize
                 AVFrame *frame = av_frame_alloc();
                 frame->format = encoder_pix_fmt;
                 frame->width = windowWidth;
                 frame->height = windowHeight;
-                frame->data[0] = dst_data[0];
-                frame->linesize[0] = dst_linesize[0];
+                frame->data[0] = dst_data[0]; // Y plane
+                frame->data[1] = dst_data[1]; // U plane
+                frame->data[2] = dst_data[2]; // V plane
+                frame->linesize[0] = dst_linesize[0]; // Y plane
+                frame->linesize[1] = dst_linesize[1]; // U plane
+                frame->linesize[2] = dst_linesize[2]; // V plane
+                frame->pts = av_rescale_q(frameIndex, encoderCodecCtx->time_base, encoderStream->time_base);
+                frame->time_base = encoderCodecCtx->time_base;
+
+                g_print("Sending frame... %d\n", frameIndex);
 
                 // Send the frame to the encoder
-                if (avcodec_send_frame(encoderCodecCtx, frame) < 0) {
-                    fprintf(stderr, "Error sending frame to encoder\n");
+                ret = avcodec_send_frame(encoderCodecCtx, frame);
+                if (ret < 0) {
+                    char errbuf[AV_ERROR_MAX_STRING_SIZE] = {0};
+                    av_make_error_string(errbuf, AV_ERROR_MAX_STRING_SIZE, ret);
+                    g_print("Error sending frame to encoder: %s\n", errbuf);
                     return;
                 }
+
+                g_print("Writing frame...\n");
 
                 // Receive the encoded packets from the encoder
                 AVPacket *packet = av_packet_alloc();
@@ -288,17 +379,34 @@ void SetupDesktopDuplication(HWND sharedHwnd) {
         desktopSurface->Release();
         desktopResource->Release();
         dxgiOutputDuplication->ReleaseFrame();
-        d3dDeviceContext->Unmap(desktopSurface, 0);
+        d3dDeviceContext->Unmap(cpuTexture, 0);
+        cpuTexture->Release();
 
         // fps throttling
         auto frameEnd = std::chrono::high_resolution_clock::now();
         auto captureDuration = std::chrono::duration_cast<std::chrono::milliseconds>(frameEnd - frameStart);
+
+        frameIndex++;
 
         if (captureDuration < frameDuration) {
             // If we finished capturing faster than our target frame rate,
             // sleep for the remaining time.
             std::this_thread::sleep_for(frameDuration - captureDuration);
         }
+    }
+
+    encoderStream->duration = frameIndex * frameDuration.count();
+
+    // Flush the encoder
+    avcodec_send_frame(encoderCodecCtx, NULL);
+    AVPacket *packet = av_packet_alloc();
+    while (avcodec_receive_packet(encoderCodecCtx, packet) == 0) {
+        // Write the encoded packet to the output file
+        if (av_write_frame(encoderFormatCtx, packet) < 0) {
+            fprintf(stderr, "Error writing packet to output file\n");
+            return;
+        }
+        av_packet_unref(packet);
     }
 
     // Write file trailer and close the file
